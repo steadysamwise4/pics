f94beb69396d214a4618cdfeb63687b0d0663c0a
steadysamwise4 <davenportsam44@gmail.com>
2023-05-30 11:47:18 -0500

    Converted App to stateful widget. Simple counter wired up to button

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,8 @@ import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 
-void main() {
-  var app = MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text("Let's See Images!"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          print('Hi there!');
-        },
-      ),
-    ),
-  );
+import 'src/app.dart';
 
-  runApp(app);
+void main() {
+  runApp(App());
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,30 @@
-// import flutter helper library
 import 'package:flutter/material.dart';
 
-// Create a class that will be our custom widget
-// This class must extend the 'StatelessWidget' base class
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
 
-// Must define a  build method that returns
-// the widgets that *this* widget will show
+class AppState extends State<App> {
+  int counter = 0;
+
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text('$counter'),
+        appBar: AppBar(
+          title: const Text("Let's See Images!"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              counter++;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
