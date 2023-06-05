f4ce1afbd21d2b1ba6ac7ac0a24b3d8663a843ca
steadysamwise4 <davenportsam44@gmail.com>
2023-06-05 08:07:40 -0400

    slight structure refactor, container/layout styling added

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -8,10 +8,30 @@ class ImageList extends StatelessWidget {
   @override
   Widget build(context) {
     return ListView.builder(
-      itemBuilder: ((context, index) {
-        return Image.network(images[index].url);
-      }),
+      itemBuilder: (context, index) {
+        return buildImage(images[index]);
+      },
       itemCount: images.length,
+    );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(
+        color: Colors.grey,
+      )),
+      padding: const EdgeInsets.all(20.0),
+      margin: const EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Image.network(image.url),
+          ),
+          Text(image.title),
+        ],
+      ),
     );
   }
 }
